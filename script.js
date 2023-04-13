55c1a850c4c36b904918627701bc6f51e094ee6d
Ravi <ravkchauhan1aip@gmail.com>
2023-04-13 23:04:10 +0530

    update

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ fill.addEventListener('dragend', dragEnd)
 
 for(const empty of empties) {
     empty.addEventListener('dragover', dragOver)
-    empty.addEventListener('dragenter', dragEnter)
     empty.addEventListener('dragleave', dragLeave)
     empty.addEventListener('drop', dragDrop)
 }
